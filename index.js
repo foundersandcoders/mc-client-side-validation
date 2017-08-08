321527759334516cf2a5d027cfbfb75c79bdffdf
rachaelcodes <rorourke.rsp@gmail.com>
2017-08-08 11:02:38 +0100

    morning challenge

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log('hello');
+var submit = document.getElementById('submit');
+var emailBox = document.getElementById('email');
+var passwordBox = document.getElementById('password');
+var confirmPasswordBox = document.getElementById('confirmPassword');
+var passwordWarning = document.getElementById('passwordWarning');
+var emailWarning = document.getElementById('emailWarning');
+var noEmailWarning = document.getElementById('noEmailWarning');
+
+function passwordCheck(password, passwordConfirm) {
+  if (password !== passwordConfirm) {
+    passwordWarning.classList.remove('invisible');
+    passwordBox.classList.add('warning');
+    confirmPasswordBox.classList.add('warning');
+  }
+};
+
+submit.addEventListener('click', function (e){
+  // e.preventDefault();
+  var email = e.path[1].elements[0].value;
+  var password = e.path[1].elements[1].value;
+  var passwordConfirm = e.path[1].elements[2].value;
+  passwordCheck(password, passwordConfirm);
+
+});
